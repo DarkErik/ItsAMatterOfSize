fec33ff2542f8ea5dd15b1c87eb8be51a642f90b
Michael Stach <Michael.Stach@ruhr-uni-bochum.de>
2021-05-15 00:59:30 +0200

    Added simple firing Script

---
+++ Assets/Scripts/Entities/Player/PlayerWeaponController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeaponController : MonoBehaviour
+{
+    private BasicWeapon basicWeapon;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        basicWeapon = GetComponent<BasicWeapon>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (basicWeapon != null && Input.GetButton("Fire1"))
+        {
+            basicWeapon.Fire(getFireDirection());
+        }
+        else {
+            //Falls ein Fehler bei Awake passieren sollte
+            basicWeapon = GetComponent<BasicWeapon>();
+        }
+    }
+
+    private Vector2 getFireDirection() {
+
+        Vector3 mousePos = Input.mousePosition;
+        Transform transform = GetComponent<Transform>();
+
+        Vector2 direcVector = new Vector2(mousePos.x - Camera.main.scaledPixelWidth / 2, mousePos.y - Camera.main.scaledPixelHeight / 2);
+
+        return direcVector;
+    }
+}
