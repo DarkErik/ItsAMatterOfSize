aa59547a3544b4b0d8bef053417d995ed018e333
Michael Stach <Michael.Stach@ruhr-uni-bochum.de>
2021-05-19 00:35:55 +0200

    Added Reg[A]Irock

--- Assets/Scripts/UI/Credits/CreditsSceneTransition.cs
+++ Assets/Scripts/UI/Credits/CreditsSceneTransition.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CreditsSceneTransition : MonoBehaviour
 {
+    [SerializeField] private GameObject regAIrock;
+    [SerializeField] private GameObject paintRegirock;
+
     public void startTransition()
     {
         SceneManager.LoadScene("TitleScreen");
@@ -15,6 +19,28 @@ public class CreditsSceneTransition : MonoBehaviour
         if (Input.GetButton("Fire1")) 
         {
             startTransition();
+        }
+    }
+
+    private void Awake()
+    {
+        ChooseRegirock();
+    }
+
+    private void ChooseRegirock() {
+        int rand = Random.Range(0, 4);
+
+        Debug.Log("Rolled a " + rand + " while selecting Regirock");
+
+        if (rand == 0)
+        {
+            regAIrock.SetActive(false);
+            paintRegirock.SetActive(true);
+        }
+        else 
+        {
+            regAIrock.SetActive(true);
+            paintRegirock.SetActive(false);
         }
     }
 }
